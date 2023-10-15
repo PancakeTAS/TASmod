95b49be830155b015f7e8c2c242fb7a11a7f5db8
PancakeTAS <pancake@mgnet.work>
2023-10-15 18:22:29 +0200

    Upload fabric project base

---
+++ src/main/java/com/minecrafttas/tasmod/TASmod.java
@@ -0,0 +1,17 @@
+package com.minecrafttas.tasmod;
+
+import lombok.Getter;
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * TASmod fabric mod core for client and server
+ * @author Pancake
+ */
+public class TASmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
---
+++ src/main/java/com/minecrafttas/tasmod/client/ClientTASmod.java
@@ -0,0 +1,19 @@
+package com.minecrafttas.tasmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+/**
+ * TASmod fabric mod core for client
+ * @author Pancake
+ */
+@Environment(EnvType.CLIENT)
+public class ClientTASmod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
